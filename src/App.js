eff2a679ddff13202136ce1fb341ae5760778faf
Mahmoud Youssef <ma7moudyoussef21@gmail.com>
2025-04-06 17:17:04 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import{ Header, Footer , Hero, Container} from './components/index';
+import './App.css';
+function App() {
+  return (
+  <>
+    <Header />
+    <Container>
+      <Hero />
+      
+    </Container>
+    {/* <Footer /> */}
+  </>
+  
+  
+  );
+}
+
+export default App;
---
+++ src/components/container/container.js
@@ -0,0 +1,14 @@
+import './container.css';
+
+const container = (props) => {
+  return (
+    <div className='container main-container'>
+      {props.children}
+      
+      
+       
+      </div>
+  )
+}
+
+export default container 
